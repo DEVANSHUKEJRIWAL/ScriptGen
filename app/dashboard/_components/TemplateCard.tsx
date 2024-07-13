6f8a20d4da11d0bdd7d50694532b1069bba0e9e4
Devanshu Kejriwal <devanshukejriwal@Devanshus-MacBook-Air.local>
2024-07-14 00:16:30 +0530

    new

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { TEMPLATE } from './TemplateListSection'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import { TEMPLATE } from './TemplateListSection';
+import Image from 'next/image';
+import Link from 'next/link';
 
-function TemplateCard(item:TEMPLATE) {
+function TemplateCard(item: TEMPLATE) {
   return (
-    <Link href={'/dashboard/content/'+item?.slug}>
-    <div className="p-5 shadow-md rounded-md border bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all">
-      <Image src={item.icon} alt="icon" width={50} height={50}/>
-      <h2 className="font-medium text-lg">{item.name}</h2>
-      <p className="text-gray-500 line-clamp-3">{item.desc}</p>
-    </div>
+    <Link href={'/dashboard/content/' + item?.slug}>
+      <div className="p-4 shadow-md rounded-md border bg-white flex flex-col gap-1 cursor-pointer hover:scale-105 transition-all" style={{ height: '200px' }}>
+        <Image src={item.icon} alt="icon" width={50} height={50} />
+        <h2 className="font-medium text-lg">{item.name}</h2>
+        <p className="text-gray-500 overflow-hidden text-ellipsis" style={{ display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical' }}>
+          {item.desc}
+        </p>
+      </div>
     </Link>
-  )
+  );
 }
 
-export default TemplateCard
+export default TemplateCard;
